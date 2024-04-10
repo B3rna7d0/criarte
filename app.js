62f05c78f4f9263eb624ac50c6cdc32469bf7e54
bernardo <bernardomedeiross@gmail.com>
2024-04-10 01:01:00 -0300

    attempt of set modal window in services cards buttons commit fodase

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Efeito animação ao abrir o site
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,3 +12,14 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+// Modal window
+function openModal() {
+    const modal = document.getElementById('window-modal')
+    modal.classList.add('opener')
+    
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'close-button' || e.target.id == 'window-modal'){
+            modal.classList.remove('opener')
+        }
+    })
+}
